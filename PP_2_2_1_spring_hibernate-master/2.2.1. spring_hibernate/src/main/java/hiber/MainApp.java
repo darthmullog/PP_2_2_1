a8f098b227ca8d37ba420c358cabedfebff7d510
darthmullog <168669499+darthmullog@users.noreply.github.com>
2024-07-29 16:25:53 +0300

    Add files via upload

---
+++ PP_2_2_1_spring_hibernate-master/2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -0,0 +1,59 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.CarService;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+
+        UserService userService = context.getBean(UserService.class);
+        CarService carService = context.getBean(CarService.class);
+
+        Car car1 = new Car("BMW", 5);
+        carService.add(car1);
+        Car car2 = new Car("Bentley", 2014);
+        carService.add(car2);
+
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+        user1.setCar(car1);
+        userService.add(user1);
+
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+        user2.setCar(car2);
+        userService.add(user2);
+        userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+        userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+        List<User> users = userService.listUsers();
+
+        for (User user : users) {
+            System.out.println("Id = " + user.getId());
+            System.out.println("First Name = " + user.getFirstName());
+            System.out.println("Last Name = " + user.getLastName());
+            System.out.println("Email = " + user.getEmail());
+
+            Car car = user.getCar();
+
+            if (car != null) {
+                System.out.println("Car Model = " + car.getModel());
+                System.out.println("Car Series = " + car.getSeries());
+            }
+            System.out.println();
+        }
+        //ниже (строки 52-56)- проверка работы метода getUserByCar
+        String model = "Bentley";
+        int series = 2014;
+
+        User user = userService.getUserByCar(model, series);
+        System.out.println("имя владельца Bentley: " + user.getFirstName());
+        context.close();
+    }
+}
